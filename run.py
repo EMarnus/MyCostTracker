e413847b2ef08678480f5dcd0abfa825f577b05d
Emile <bvcxzq1@gmail.com>
2023-03-15 21:24:28 +0000

    Add function to push data to googlesheet, nneeds validation and testing

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+from dateutil import parser
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -12,17 +13,47 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('costTracker')
 
-data_worksheet = SHEET.worksheet('data')
 
-data = data_worksheet.get_all_values()
+def get_data():
+    """
+    Pull stored data from google sheet
+    """
 
-data_worksheet.append_row()
 
-print(data)
 
 
 #Add item - Get working and then add try fail states
+def add_data():
+    """
+    Add data to the google sheet
+    """
+    new_row = [] #Append to this
 
+    name = input("Enter Item Name: ")
+    new_row.append(name)
+
+    cost = input("Enter the cost (Don't use symbom): ")
+    new_row.append(cost)
+    
+    try:
+        tax = input("Enter tax rate for the item (Don't use %)")
+    except:
+        print("Please don't enter a symbol")
+    new_row.append(tax)
+
+    try:
+        margin = input("Enter margin rate for the item (Don't use %)")
+    except:
+        print("Please don't enter a symbol")
+    new_row.append(margin)
+
+    new_row.append("FALSE")
+
+    worksheet_to_update = SHEET.worksheet('data')
+    worksheet_to_update.append_row(new_row)
+
+    print(new_row)
+    new_row.append(new_row)
 
 
 
@@ -43,10 +74,19 @@ print(data)
 
 #Calculate Margin - Get working and then add try fail states
 
+add_data()
 
 
+"""
 
+Look at adding dates,     
 
-print("What would you like to do?")
-print()
-print("Options")
+import datetime
+
+input_date = parser.parse(input("Enter a date in DD/MM/YYYY format: "))
+date = input_date.date()
+
+date_entry = input('Enter a date in YYYY-MM-DD format')
+year, month, day = map(int, date_entry.split('-'))
+date1 = datetime.date(year, month, day)
+"""
