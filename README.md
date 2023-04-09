94ffbc4d8ec4d957f23c14833af279c01f64be89
Emile <bvcxzq1@gmail.com>
2023-04-09 18:33:05 +0000

    Correcting PEP8 errors from CI Python Linter

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ My Cost Calculator is a program to project managers to easily keep track of ongo
 For example, Love Maths is a site that hopes to demonstrate how pure JavaScript works in a real-world context. The site will be targeted toward people who not only love to implement more advanced JavaScript concepts but also maths. Love Maths is a fully responsive JavaScript maths game that will allow users to add, subtract, multiply and divide numbers. 
 
 [Live site Link](https://cost-tracker-ci.herokuapp.com/)
-![Workflow design](https://lucid.app/documents/embedded/e756fa59-d122-44f1-85af-31b39b35ce3f?invitationId=inv_7a6ed2b7-b615-4b97-ba5c-a47263def9a8#)
+[Workflow design](https://lucid.app/documents/embedded/e756fa59-d122-44f1-85af-31b39b35ce3f?invitationId=inv_7a6ed2b7-b615-4b97-ba5c-a47263def9a8#)
 
 ## Features 
 
@@ -17,16 +17,16 @@ The objects add will be stored and reteived from an external database.
 ### User Stories
 #### Visitor Goals
 - As a first time user, I want easily understand the main purpose of the website.
-- Opening text explains function and acttions that can be taken.
+  - Opening text explains function and acttions that can be taken.  
 
 - As a first time user, I want to be able to easily see the options.
-- On loading you are presented with a list of options.
+  - On loading you are presented with a list of options.  
 
 - As a first time user, I want to be able to select set VAT and Margin rates for each cost.
-- Each item has an associated VAT and Margin rate that is entered with the item.
+  - Each item has an associated VAT and Margin rate that is entered with the item.  
 
 - As a returning user, I want to be able retreive previously entered items.
-- There is an option to print all saved items.
+  - There is an option to print all saved items.  
 
 
 ### Existing Features
@@ -46,17 +46,9 @@ The objects add will be stored and reteived from an external database.
 
 ### Validator Testing 
 
-- HTML
+- Python
     - No errors were returned when passing through the official [W3C validator](https://validator.w3.org/nu/?doc=https%3A%2F%2Fcode-institute-org.github.io%2Flove-maths%2F)
-- CSS
-    - No errors were found when passing through the official [(Jigsaw) validator](https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fvalidator.w3.org%2Fnu%2F%3Fdoc%3Dhttps%253A%252F%252Fcode-institute-org.github.io%252Flove-maths%252F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en)
-- JavaScript
-    - No errors were found when passing through the official [Jshint validator](https://jshint.com/)
-      - The following metrics were returned: 
-      - There are 11 functions in this file.
-      - Function with the largest signature takes 2 arguments, while the median is 0.
-      - Largest function has 10 statements in it, while the median is 3.
-      - The most complex function has a cyclomatic complexity value of 4 while the median is 2.
+
 
 ## Testing & fixed bugs
 
--- run.py
+++ run.py
@@ -29,32 +29,32 @@ def inputNumber(message):
     """
     while True:
         try:
-            userInput = int(input(message))       
+            userInput = int(input(message))
         except ValueError:
             print("\nNot a number, please enter a number.")
             continue
         else:
-            return userInput 
-            break 
+            return userInput
+            break
 
 
-#Add item - Get working and then add try fail states
+# Add item - Get working and then add try fail states
 def add_data():
     """
     Add data to the google sheet
     """
-    new_row = [] #Append to this
+    new_row = []  # Append to this
 
     name = input("Enter Item Name: \n")
     new_row.append(name)
 
     cost = inputNumber("Enter the cost (Please don't use symbols): \n")
     new_row.append(cost)
-    
+
     tax = inputNumber("Enter tax rate for the item (Please don't use symbols, \
 20% > 20): \n")
     new_row.append(tax)
-        
+
     margin = inputNumber("Enter margin rate for the item (Please don't use \
 symbols, 5% > 5): \n")
     new_row.append(margin)
@@ -72,19 +72,18 @@ symbols, 5% > 5): \n")
     main()
 
 
-
-#Print all objects - Get working and then add try fail states
-"""
-Prints all rows in googlesheets, only print if Hidden set to false. 
-Item    Cost    tax     margin
-Example from https://www.educba.com/python-print-table/ 
-Also from stackoverflow https://stackoverflow.com/questions/42235918/python-tabulate-dictionary-containing-two-values-per-key
-
-"""
+# Print all objects - Get working and then add try fail states
 def print_table():
+    """
+    Prints all rows from googlesheets, only print if Hidden set to false.\
+ Example from https://www.educba.com/python-print-table/
+    Also from stackoverflow https://stackoverflow.com/questions/42235918\
+/python-tabulate-dictionary-containing-two-values-per-key
+    """
     base_data = calc_margin()
-    new_data = [{k: v for k, v in dictionary.items() if k != 'hidden'} for dictionary in base_data if dictionary['hidden'] == 'FALSE']
-    
+    new_data = [{k: v for k, v in dictionary.items() if k != 'hidden'} 
+for dictionary in base_data if dictionary['hidden'] == 'FALSE']
+
     headers = new_data[0].keys()
     values = [list(dictionary.values()) for dictionary in new_data]
 
@@ -94,26 +93,25 @@ def print_table():
     main()
 
 
-
-
-#Delete an object - Get working and then add try fail states
+# Delete an object - Get working and then add try fail states
 def del_item():
     """
     Prints current items and lets you select an item to hide.
     """
     base_data = get_data()
-    
-    #prints to delete from items to select
+
+    # prints to delete from items to select
     for item in base_data:
         print(item.get("item").capitalize())
 
     to_delete = input("Please enter the item name from the list above that\
  you would like to delete: \n").lower()
 
-
     worksheet_to_update = SHEET.worksheet('data')
 
-    #credit for code snip is https://stackoverflow.com/questions/71029282/update-value-in-google-sheet-with-if-condition-in-another-column-using-python
+# credit for code snip is
+# https://stackoverflow.com/questions/71029282/update-
+# value-in-google-sheet-with-if-condition-in-another-column-using-python
     records_data = worksheet_to_update.get_all_records()
     test = worksheet_to_update.col_values(1)
     rownum = test.index('pen') + 1
@@ -125,44 +123,42 @@ def del_item():
     main()
 
 
-
-
-
-#Calculate VAT - Get working and then add try fail states
+# Calculate VAT - Get working and then add try fail states
 def calc_gross():
     """
-    Gets the data from the google sheet, applies the set VAT and then returns result
+    Gets the data from the google sheet, applies the set VAT and then \
+returns result.
     """
 
     base_data = get_data()
     working_data = copy.deepcopy(base_data)
 
     for index in range(len(working_data)):
-        working_data[index]["gross"] = working_data[index]["cost"] + (working_data[index]["cost"] * (working_data[index]["tax"]/100))
-     
+        working_data[index]["gross"] = working_data[index]["cost"] + \
+(working_data[index]["cost"] * (working_data[index]["tax"]/100))
+
     return working_data
 
 
-
-
-
-
-#Calculate Margin - Get working and then add try fail states
+# Calculate Margin - Get working and then add try fail states
 def calc_margin():
     """
-    Gets the data from the google sheet, applies the set VAT, calculates the price with Margin then returns result
+    Gets the data from the google sheet, applies the set VAT, calculates the \
+price with Margin then returns result
     """
     working_data = calc_gross()
 
     for index in range(len(working_data)):
-        working_data[index]["price"] = working_data[index]["gross"] + (working_data[index]["gross"] * (working_data[index]["margin"]/100))
+        working_data[index]["price"] = (working_data[index]["gross"] + 
+(working_data[index]["gross"] * (working_data[index]["margin"]/100)))
 
     return working_data
 
 
 def main():
     """
-    The function the governs user interaction, letting them choose what they want to do.
+    The function the governs user interaction, letting them choose what they \
+want to do.
     """
     action = inputNumber("Welcome to my little program. It allows you to keep\
  track of your project's \ncosts and have individual VAT and margin \
@@ -170,8 +166,8 @@ percentages.\n\n1. Enter new data\n2. Print data\n3. Delete entered item\
 \n\nPlease select what you want to do by entering a number \
 from the list above: \n")
 
-    #action = int(action)
-    
+# action = int(action)
+
     if action == 1:
         add_data()
     elif action == 2:
@@ -183,12 +179,10 @@ from the list above: \n")
         main()
 
 
-
 main()
 
 """
-
-Look at adding dates,     
+Look at adding dates
 
 import datetime
 
